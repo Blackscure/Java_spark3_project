fe93cde2cb615b26314044805a50fa12d083621e
Blackscure <Wekesabuyahi@gmail.com>
2020-05-06 13:19:49 +0300

    test for intatiating animal

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,2 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class AnimalTest {
+    @Test
+    public void instantiatesAnimal_true() {
+        Animal  testAnimal = new Animal( "Tiger");
+        assertEquals(true, testAnimal instanceof Animal);
+   }
 }
