958701a61c79645161b502bc92e07ef17ef87398
Blackscure <Wekesabuyahi@gmail.com>
2020-05-06 13:44:21 +0300

    @ data base rule

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,7 +1,10 @@
+import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
     @Test
     public void instantiatesAnimal_true() {
         Animal  testAnimal = new Animal( "Tiger");
