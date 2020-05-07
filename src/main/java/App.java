2ae3fb57e44b818ea2980dc184050170106bd9b1
Blackscure <Wekesabuyahi@gmail.com>
2020-05-07 10:06:50 +0300

    get methods added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,8 @@
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
     ProcessBuilder process = new ProcessBuilder();
     Integer port;
@@ -7,4 +12,12 @@ public class App {
     } else {
         port = 4567;
     }
+
+    port(port);
+    staticFileLocation("/public");
+    get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        return new ModelAndView(model, "index.hbs");
+    }, new HandlebarsTemplateEngine());
+
 }
