4e16ab366ee6111cd56dab1ab29e00c7c362220c
Blackscure <Wekesabuyahi@gmail.com>
2020-05-07 10:05:15 +0300

    initializing our port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,10 @@
 public class App {
+    ProcessBuilder process = new ProcessBuilder();
+    Integer port;
+
+        if (process.environment().get("PORT") != null) {
+        port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+        port = 4567;
+    }
 }
